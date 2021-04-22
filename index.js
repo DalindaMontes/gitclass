8a77baa19e860253966902883d165ede7801dc68
Dalinda Montes <dalindam6@gmail.com>
2021-04-21 20:30:48 -0500

    se agregaron archivos de la tarea

---
+++ index.js
@@ -0,0 +1,28 @@
+var global ="Global";
+const global = "constante";
+
+function example (){
+    let global = "Local";
+    console.log("2" + global1)
+    console.log(global);
+}
+
+function suma (num1, num2){
+    console.log(num1 + num2);
+}
+
+function resta (num1, num2){
+    console.log(num1 - num2);
+}
+
+function multiplicacion (num1, num2){
+    console.log(num1 * num2);
+}
+
+function division (num1, num2){
+    console.log(num1 / num2);
+}
+suma(10,4);
+resta(10,4);
+multiplicacion(10,4);
+division(10,4);
